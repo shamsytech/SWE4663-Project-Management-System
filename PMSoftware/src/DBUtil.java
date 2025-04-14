e53708d677415622a5b8a223ead7612807f18725
Harry Nguyen <143207451+Harry7721@users.noreply.github.com>
2025-04-14 17:04:00 -0400

    Adding database connection to the project

---
+++ PMSoftware/src/DBUtil.java
@@ -0,0 +1,15 @@
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DBUtil {
+    public static Properties loadDBProperties() {
+        Properties props = new Properties();
+        try {
+            FileInputStream fis = new FileInputStream("db.properties");
+            props.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
+}
--- PMSoftware/src/Main.java
+++ PMSoftware/src/Main.java
@@ -1,5 +1,21 @@
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        Properties props = DBUtil.loadDBProperties();
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+
+        Connection conn = DriverManager.getConnection(url, user, password);
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Connected successfully!");
+            conn.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
